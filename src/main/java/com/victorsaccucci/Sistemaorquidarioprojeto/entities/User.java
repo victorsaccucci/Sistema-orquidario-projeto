f2d8393b214e4fb6eafd7b39a89c4fcca9ff0303
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-17 17:12:32 -0300

    Create user entity

--- src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/User.java
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/User.java
@@ -1,4 +1,29 @@
 package com.victorsaccucci.Sistemaorquidarioprojeto.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+    private String instagram;
+
+    public User(Long id, String name, String email, String password, String phone, String instagram) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.instagram = instagram;
+    }
+    public User(){
+    }
 }
