7edaa9420ddc65175cabf1357830a77addbb84ab
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-17 16:58:45 -0300

    Initial commit

---
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/SistemaOrquidarioProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.victorsaccucci.Sistemaorquidarioprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaOrquidarioProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaOrquidarioProjetoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/User.java
@@ -0,0 +1,4 @@
+package com.victorsaccucci.Sistemaorquidarioprojeto.entities;
+
+public class User {
+}
