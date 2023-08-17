6852c0024b1a1bbcfd9fd77fdb41f2088cd0b7c2
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-17 17:31:53 -0300

    Created all entities

--- src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/Galery.java
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/Galery.java
@@ -1,4 +1,30 @@
 package com.victorsaccucci.Sistemaorquidarioprojeto.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Galery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+    private String subtitle;
+
+    public Galery(Long id, User user, String title, String subtitle) {
+        this.id = id;
+        this.user = user;
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+    public Galery(){
+    }
 }
--- src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/Species.java
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/entities/Species.java
@@ -1,4 +1,33 @@
 package com.victorsaccucci.Sistemaorquidarioprojeto.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.java.Log;
+@Getter
+@Setter
+@Entity
 public class Species {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "galery_id")
+    private Galery galery;
+
+    private String identification;
+    private String description;
+    @Lob
+    private byte[] image;
+
+    public Species(Long id, Galery galery, String identification, String description, byte[] image) {
+        this.id = id;
+        this.galery = galery;
+        this.identification = identification;
+        this.description = description;
+        this.image = image;
+    }
+    public Species(){
+    }
 }
