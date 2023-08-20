8cb0a156b7718573c5c97b91b47e9f636c0e2861
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-20 17:25:47 -0300

    Created menu back and front-end

---
+++ src/main/java/com/victorsaccucci/Sistemaorquidarioprojeto/controllers/MenuController.java
@@ -0,0 +1,15 @@
+package com.victorsaccucci.Sistemaorquidarioprojeto.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MenuController {
+
+        @GetMapping(value = "/better")
+        public String better() {
+            return "destaques";
+        }
+
+}
